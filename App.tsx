37856d91a465f4e3fc876cfbbd2999b139728128
Sathya JothiL <sathyajothi845@gmail.com>
2025-06-05 11:39:03 +0530

    intial

---
+++ App.tsx
@@ -0,0 +1,18 @@
+import { Platform, StyleSheet, Text, View } from 'react-native'
+import React, { useEffect } from 'react'
+// import SplashScreen from 'react-native-splash-screen'
+
+const App = () => {
+  // useEffect(()=>{
+  //   if(Platform.OS==='android') SplashScreen.hide()
+  // },[])
+  return (
+    <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
+      <Text style={{color:'red'}}>Oh my god</Text>
+    </View>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
